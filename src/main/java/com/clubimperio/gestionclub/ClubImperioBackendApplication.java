d1a065ba25af5ef28ed455c5a4cf88432bd35e5c
UrielIvanBarrios <barriosurielivan@gmail.com>
2026-03-17 19:27:41 -0300

    Initial commit: Configuración base de Spring Boot y seguridad de properties

---
+++ src/main/java/com/clubimperio/gestionclub/ClubImperioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clubimperio.gestionclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubImperioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubImperioBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clubimperio/gestionclub/ClubImperioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubimperio.gestionclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubImperioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
